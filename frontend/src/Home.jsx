61368f5d78001970dd9cca755ebc6d5dc1e0c87b
Lydia.Chen <szupei21@gmail.com>
2025-06-18 01:10:20 +0800

    feat: Home.jsx

---
+++ frontend/src/Home.jsx
@@ -0,0 +1,12 @@
+function Home() {
+  return (
+    <div style={{ padding: '2rem', textAlign: 'center' }}>
+      <h1>Hi，我是信義小魯</h1>
+      <p>歡迎來到我的個人網站！</p>
+      <p>我提供最優質的購屋服務！</p>
+      <a href="#projects">👉 查看我的物件</a>
+    </div>
+  );
+}
+
+export default Home;
